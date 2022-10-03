252f959ef0e612318776070a4d0664ca25fe6cbc
Arthur Anastopulos <arthur.santos@khomp.com>
2022-10-03 12:27:04 -0300

    fix(client-tftp): Corrigido RRQ para que o ACK seja enviado para o servidor corretamente.

--- client/clientTftp.py
+++ client/clientTftp.py
@@ -24,7 +24,6 @@ class ClientTFTP(poller.Callback):
 
         self.__socket = socket(AF_INET, SOCK_DGRAM)
         poller.Callback.__init__(self, self.__socket, self.__tout)
-        self.__state =  self.__handle_idle
 
         self.__n = 0
     
@@ -91,15 +90,6 @@ class ClientTFTP(poller.Callback):
         sched.adiciona(self)
         sched.despache()
 
-    """Mudança de Estado para Ocioso
-
-    @param msg: Messagem utilizadas durante a troca de messagem
-    @param tout: verifcação de ocorrencia do timeout
-    """
-    def __handle_idle(self, msg:Message):
-        print("Idle")
-        self.__state = self.__handle_idle
-
     """Mudança de Estado para Conectando
 
     @param msg: Messagem utilizadas durante a troca de messagem
@@ -113,7 +103,7 @@ class ClientTFTP(poller.Callback):
             print("Obteve Erro no Connect")
             error = Error(5, msg.getBuffer()[3])
             print(error.getErrorMsg())
-            self.__state = self.__handle_idle
+            sys.exit()
 
         #Se Receber um DATA (Significa que é RX)
         if(msg.getOpcode() == 3 ):
@@ -156,13 +146,12 @@ class ClientTFTP(poller.Callback):
         if( msg.getOpcode() == 5 ):
             error = Error(5, msg.getBuffer()[3])
             print(error.getErrorMsg())
-            self.__state = self.__handle_idle
+            sys.exit()
         
 
         #Recebe Msg do socket
         data = msg.getBuffer()[4:]
         data_block_m = int.from_bytes(msg.getBuffer()[2:4],"big")
-
         #Se len do Data == 512, continua neste estado
             #envia Ack, incrementa n
         if( (msg.getOpcode() == 3) and (len(data) == 512) ):
@@ -183,7 +172,9 @@ class ClientTFTP(poller.Callback):
             sendMsg = Ack(4, block_n)
             self.__socket.sendto( sendMsg.serializeMsg(), (self.__ip,self.__port) )
             self.__file.write(data)
-            self.__state = self.__handle_end
+            print("Transferencia Concluida")
+            self.__file.close()
+            sys.exit() 
         
     
     """Mudança de Estado para Transmissão
@@ -224,33 +215,28 @@ class ClientTFTP(poller.Callback):
         if( msg.getOpcode() == 5 ):
             error = Error(5, msg.getBuffer()[3])
             print(error.getErrorMsg())
-            self.__state = self.__handle_idle
+            sys.exit()
 
-        #Para Finalização de RX
-        if(msg.getBuffer() == None):
-            print("Transferencia Concluida")
-            self.__file.close()
-            self.__state = self.__handle_idle
-        elif( ( msg.getOpcode() == 3 ) and ( len(msg.getBuffer()[4:]) ) ):
+        if( ( msg.getOpcode() == 3 ) and ( len(msg.getBuffer()[4:]) ) ):
             block_n = struct.pack(">H",self.__n)
             sendMsg = Ack(4, block_n)
             self.__socket.sendto( sendMsg.serializeMsg(), (self.__ip,self.__port) )
             self.__file.close()
-            self.__state = self.__handle_idle
+            sys.exit()
         elif( (msg.getOpcode() == 4) and ( msg.getBuffer() != None) ):
             self.__n += 1
             sendData = self.__file.read(512)
             block_n = struct.pack(">H", self.__n)
             dataMsg = Data(3, block_n, sendData)
             self.__socket.sendto( dataMsg.serializeMsg(), (self.__ip,self.__port) )
-            self.__state = self.__handle_idle
+            sys.exit()
 
 
     def handle(self):
         #recebe mensagem do socket
        
         data, (addr, port) = self.__socket.recvfrom(516) # 512 bytes + opcode + block
-
+        self.__port = port  
         #transformar data em objeto mensagem
         if type(data) is bytes:
             print( "Recebido Mensagem do " + str(addr) + ":" + str(port) + " = " )
@@ -269,5 +255,4 @@ class ClientTFTP(poller.Callback):
         print("Timeout")
         self.disable_timeout()
         self.disable()
-        self.__state = self.__handle_idle
         #maquina de estado passar para ociso
